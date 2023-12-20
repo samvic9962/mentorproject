26210ced5eac93fb0e1c92bbe51b7e091d8ab28a
keerthanaduari <146316204+keerthanaduari@users.noreply.github.com>
2023-12-20 14:04:15 +0530

    Add files via upload
    travelbookingapplication

---
+++ TravelBookingApplication.java
@@ -0,0 +1,14 @@
+package com.example.travelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBookingApplication.class, args);
+		System.out.println("welcome to travelbooking");
+	}
+
+}
